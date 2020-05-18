d17f9546c371ce6d69c90907d087980f773296de
Ivan <jvin08@gmail.com>
2020-05-17 21:19:40 -0500

    Initial commit: Added method testCountLetters

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,20 +1,30 @@
 import org.testng.annotations.Test;
 
 public class FirstClass {
+
     @Test
     public void test00001() {
-        System.out.println("welcome to java back");
+        System.out.println("This will be printed");
     }
 
     @Test
-    public void test00003() {
-        String messageOne ="String for method 00003";
+    public void test0002() {
+        int myNumber = 5;
 
-        String messageTwo = "string two two!";
+        System.out.println(myNumber);
+    }
 
-        String messageThree = messageOne + messageTwo;
 
-        System.out.println(messageThree);
+    @Test
+    public void test0003() {
+
+        String messageOne = new String("Who let the dogs out?");
+
+        String messageTwo = "Who who who who!";
+
+        String completeMessage = messageOne + messageTwo;
+
+        System.out.println(completeMessage);
     }
 
     @Test
@@ -29,9 +39,8 @@ public class FirstClass {
     public void test005() {
         boolean toBe = false;
 
-        boolean b = true;
-        if (b == false) System.out.println(!toBe);
-        else {
+        boolean b = toBe || !toBe;
+        if (b) {
             System.out.println(toBe);
         }
 
@@ -48,27 +57,107 @@ public class FirstClass {
     //TODO: change to accept parameters so it will always print correct message
     @Test
     public void test006() {
-        System.out.println("test006");
-        int a = 4;
+        int a = 5;
 
-        if (a == 4) System.out.println("Ohhh! So a is four!!!");
-        else {
+        if (a == 5) {
+            System.out.println("Ohhh! So a is 4!");
+        } else {
             System.out.println("A IS NOT EQUAL TO 4");
+        }
+    }
+
+    public void printAllMembersOfArray(int[] arrayParameter) {
+        for (int i = 0; i < arrayParameter.length; i++) {
+            int currentElement = arrayParameter[i];
+            System.out.println(currentElement);
+        }
+    }
+
+    public void printAllMembersOfArrayE(int[] arrayParameter) {
+        for (int eachMember : arrayParameter) {
+            System.out.println(eachMember);
         }
     }
 
     @Test
     public void test007() {
-        int[] arrayOfIntegers = {1, 9, 9, 5};
+        int[] input = {1, 9, 9, 5};
+        printAllMembersOfArrayE(input);
+    }
 
-        for (int currentElement : arrayOfIntegers) {
-            System.out.println("current element is: " + currentElement);
+
+    @Test
+    public void testPrintEvenNumbers() {
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) System.out.println(i);
         }
     }
 
     @Test
-    public void test008() {
-        System.out.println("Method number 0081");
+    public void printHelloAtEvenNumbers() {
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) System.out.println("hello world");
+        }
+    }
+
+    //Swap two numbers in an array
+    //1. create an input array
+    //2. create a method with int[] parameter
+    //3. new method will also take indexes of L and R elements
+    //4. use indexes R and L and respective elements will be swapped with each other
+    //5. print out final version of the array
+    @Test
+    public void test_swapNumbers() {
+        int[] input = {1, 2, 3, 4, 5, 6};
+        int indexL = 0;
+        int indexR = 1;
+
+        swap(input, indexL, indexR);
+        printAllMembersOfArray(input);
+    }
+
+    @Test
+    public void test_swap_Exception() {
+        int[] input = {1, 2, 3, 4, 5, 6};
+        int indexL = -1;
+        int indexR = 4;
+
+        checkInputs(input, indexL, indexR);
+        swap(input, indexL, indexR);
+        printAllMembersOfArray(input);
+    }
+
+    //TODO: add checks for input array
+    private void checkInputs(int[] input, int indexL, int indexR) {
+        if (indexL >= 0 && indexL < input.length && indexR >= 0 && indexR < input.length) {
+            System.out.println("Inputs are OK");
+
+        } else {
+            throw new AssertionError("Inputs are NOT OK: indexL=" + indexL + ", indexR=" + indexR);
+        }
+    }
+
+    private void swap(int[] input, int indexL, int indexR) {
+        int temporaryElement = 0;
+        temporaryElement = input[indexL];
+        input[indexL] = input[indexR];
+        input[indexR] = temporaryElement;
+    }
+
+    //TODO: break down into sub-steps (external methods), `letter` should be a parameter as well
+    @Test
+    public void testCountLetters () {
+        char letter = 'l';
+        String input = "hello world";
+        countLetters(input, letter);
+    }
+
+    public void countLetters(String input, char letter) {
+        int result = 0;
+        char[] charArray = input.toCharArray();
+        for (char eachChar : charArray) {
+            if (eachChar == letter) result++;
+        }
+        System.out.println(result);
     }
 }
-
